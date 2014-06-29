e32326c15ff2feff7ebc6a88edb40608e799db65
KITE <ixkaito@gmail.com>
2014-06-29 15:58:08 +0900

    Add map to index.html

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/_scss"
+images_dir = "assets/images"
+javascripts_dir = "assets/js"
+# line_comments = false
+# output_style = :compact
+output_style = :compressed
+# sass_options = { :debug_info => true }
