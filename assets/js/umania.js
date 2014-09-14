24ee8db3210c759e5403ab02798bc1aeda3a250c
Yosuke ONOUE <onoue@likr-lab.com>
2014-09-14 16:32:57 +0900

    Add alerm and notification

--- assets/js/umania.js
+++ assets/js/umania.js
@@ -17,7 +17,7 @@ angular.module('umania', ['ui.router'])
 	.config(function($compileProvider) {
 		$compileProvider.imgSrcSanitizationWhitelist(/^.*/);
 	})
-	.run(function($rootScope) {
+	.run(function($rootScope, $window) {
 		$rootScope.loading = false;
 
 		$rootScope.$on('$stateChangeStart', function() {
@@ -27,4 +27,23 @@ angular.module('umania', ['ui.router'])
 		$rootScope.$on('$viewContentLoaded', function() {
 			$rootScope.loading = false;
 		});
+
+		if ($window.navigator.mozAlarms) {
+			var req;
+			var alarmDate = new Date(Date.now() + 10000); // 10秒後
+			req = $window.navigator.mozAlarms.add(alarmDate, 'honorTimezone', {});
+
+			$window.navigator.mozSetMessageHandler("alarm", function (mozAlarm) {
+				var notification = new Notification('Umania', {
+					body: 'UMAに遭遇しました',
+					icon: 'app://53f82159-2c63-ce47-b284-f4302a984232/assets/images/icon-128.png'
+				});
+				notification.onclick = function() {
+					 $window.navigator.mozApps.getSelf().onsuccess = function(evt) {
+						 var app = evt.target.result;
+						 app.launch();
+					 };
+				};
+			});
+		}
 	});
