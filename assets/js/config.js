65f41e08d3bb04ed595d16ee7d7cc428fbf48d74
Yosuke ONOUE <onoue@likr-lab.com>
2014-09-16 02:54:53 +0900

    Update config

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,4 +2,4 @@ angular.module('umania')
 	.value('notificationLimit', 5)
 	.value('sparqlEndpoint', 'http://lodcu.cs.chubu.ac.jp/SparqlEPCU/api/umania')
 	// .value('sparqlEndpoint', 'http://db.lodosaka.jp/sparql?default-graph-uri=http;//lodosaka/firefoxos/UMA')
-	.value('alarmInterval', 60000);
+	.value('alarmInterval', 3600000); // 60分
